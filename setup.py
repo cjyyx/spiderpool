0b9dba85f67f777b6d405f0fe09c08a0f10f46f4
cjyyy <1627814505@qq.com>
2023-07-27 13:23:49 +0300

    添加 setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spiderpool",
+    version="0.1",
+    packages=["spiderpool"],
+    install_requires=[],
+    entry_points={},
+)
+
+
--- spiderpool/__init__.py
+++ spiderpool/__init__.py
@@ -1,6 +1,6 @@
 '''
 测试版
-2021年1月23日19:18:42
+2023年7月24日22:50:50
 '''
 from .pool import *
 from .spidertools import*
